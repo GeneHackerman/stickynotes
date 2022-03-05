3e90b79011c82e03c98c1620d4d282c76d8600ad
Gene Hackerman <arias.rafael.1984@gmail.com>
2022-03-05 12:39:43 -0500

    currently facing module error

--- Develop/routes/apiRoutes/noteRoutes.js
+++ Develop/routes/apiRoutes/noteRoutes.js
@@ -4,23 +4,23 @@ const router = require('express').Router();
 const { notes } = require('../../db/db');
 
 // creates or deletes a note
-const { noteCreateNewNote, noteDeleteNote } = require('./lib/notefunctions');
+const { noteCreateNewNote, noteDeleteNote } = require('./assets/index.js');
 
 //save note history json db
-router.get('/notes', (req, res) => {
+router.get('api/notes', (req, res) => {
     let saved = notes;
     res.json(saved);
 });
 
-router.post('/notes', (req, res) => {
+router.post('api/notes', (req, res) => {
     //req.body holds parameters that are sent from client as POST request
     req.body.id = notes.length.toString();
     let note = noteCreateNewNote(req.body, notes);
     res.json(note);
 });
 
-router.delete('/notes/:id', (req, res) => {
-    noteDeleteNotes(notes, req.params.id);
+router.delete('api/notes/:id', (req, res) => {
+    noteDeleteNote(notes, req.params.id);
     res.json(notes);
 });
 
