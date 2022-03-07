acbc353f99d16db0c43b1996e9dd15be281bda7b
Gene Hackerman <arias.rafael.1984@gmail.com>
2022-03-06 20:17:46 -0500

    further refactor

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -1,6 +1,10 @@
 const path = require('path');
 const router = require('express').Router();
 
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../db/db.json'))
+});
+
 router.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../../db/db.json'))
 });
